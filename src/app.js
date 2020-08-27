fee10ce0d4d3d16cdf2215f1e84a70c6e5ff670b
Christian Dev <dev.chrisweb@gmail.com>
2020-08-26 23:34:55 -0500

    Add varible global

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+   // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 });
 
